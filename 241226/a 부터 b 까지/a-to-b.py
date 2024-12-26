2e58ef4786c56842f08aa637bd09f7147f177a34
박성현 <101372694+KKDDDS@users.noreply.github.com>
2024-12-26 23:51:22 +0900

    Title: a 부터 b 까지, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241226/a 부터 b 까지/a-to-b.py
@@ -0,0 +1,17 @@
+a, b = map(int, input().split())
+print(a, end=' ')
+
+while a < b:
+    
+    if a %2 ==1:
+        a *= 2
+        if a>b:
+            break
+        print(a, end=' ')
+    
+    else:
+        a += 3
+        if a>b:
+            break
+        print(a, end=' ')
+    
