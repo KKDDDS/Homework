c4e81054d8d9fea2bdab1b00214ee95527d08dfe
박성현 <101372694+KKDDDS@users.noreply.github.com>
2024-12-26 23:17:49 +0900

    Title: 100점 도달하기, Time: 41ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241226/100점 도달하기/reach-100-point.py
@@ -0,0 +1,21 @@
+n = int(input())
+
+while n < 60:
+    print("F", end=' ')
+    n +=1
+
+while n < 70:
+    print("D", end=' ')
+    n +=1
+
+while n < 80:
+    print("C", end=' ')
+    n +=1
+
+while n < 90:
+    print("B", end=' ')
+    n +=1
+
+while n <= 100:
+    print("A", end=' ')
+    n +=1
