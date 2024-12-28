b37a3cfb2429fe2e429d2ce4e64894a058dd629b
박성현 <101372694+KKDDDS@users.noreply.github.com>
2024-12-28 16:23:18 +0900

    Title: 체크 다이아몬드, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241228/체크 다이아몬드/check-diamond.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+for i in range(n):
+    for j in range(n-1-i, 0, -1):
+        print(" ",end='')
+    for k in range(i+1):
+        print("*",end=' ')
+    print()
+
+for i in range(n-1, 0, -1):
+    for l in range(n-i):
+        print(" ",end='')
+    for m in range(i):
+        print("*",end=' ')
+    print()
