947ab0af710390c948ce9a0b3366efe3b948b2ac
박성현 <101372694+KKDDDS@users.noreply.github.com>
2024-12-29 01:56:03 +0900

    Title: 규칙을 찾아 출력하기, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241229/규칙을 찾아 출력하기/find-a-rule-and-print.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        if j==1 or j==n or i==1 or i==n:
+            print("*",end=' ')
+        elif j<i:
+            print("*", end=' ')
+        else:
+            print(" ", end=' ')
+    print()
+
