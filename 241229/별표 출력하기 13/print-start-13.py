3347d1a1a91ab5ad8d62fb1bed6a89bbd4f337ec
박성현 <101372694+KKDDDS@users.noreply.github.com>
2024-12-29 00:42:18 +0900

    Title: 별표 출력하기 13, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

--- 241229/별표 출력하기 13/print-start-13.py
+++ 241229/별표 출력하기 13/print-start-13.py
@@ -1,7 +1,7 @@
 n = int(input())
 cnt1 = 0
 cnt2 = 1
-for i in range(n, 0, -1):
+for i in range(1, n+1):
     if i%2 ==1:
         for j in range(n-cnt1):
             print("*", end=' ')
@@ -16,10 +16,11 @@ for i in range(n, 0, -1):
     
     print()
 
+
 cnt1 -= 1
 cnt2 -= 1
 
-for l in range(1, n+1):
+for l in range(n,0,-1):
     if l%2 ==1:
         for m in range(n-cnt1):
             print("*", end=' ')
