73c778dfd94d3596bb6b62c65afd2304c10bad22
박성현 <101372694+KKDDDS@users.noreply.github.com>
2024-12-29 16:32:59 +0900

    Title: 3n + 1 수열 m번, Time: 49ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241229/3n + 1 수열 m번/3n-plus-1-sequence-m-times.py
@@ -0,0 +1,12 @@
+m = int(input())
+cnt = 0
+for i in range(m):
+    n = int(input())
+    while n != 1:
+        if n%2 ==0:
+            n //= 2
+        else:
+            n = n*3+1
+        cnt += 1
+    print(cnt)
+    cnt = 0
