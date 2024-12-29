491ae672577e29efcd609239b0f1b62f12f51e5d
박성현 <101372694+KKDDDS@users.noreply.github.com>
2024-12-30 04:04:29 +0900

    Title: 나눗셈의 나머지, Time: 74ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 241230/나눗셈의 나머지/remainder-of-division.py
+++ 241230/나눗셈의 나머지/remainder-of-division.py
@@ -1,10 +1,14 @@
 a, b = map(int, input().split())
+arr = [a]
 
 rst = [0]*4
-while a >1:
-    k = a%b
-    rst[k] += 1
-    a //= b
+for i in arr:
+    if i>1:
+        k = i%b
+        rst[k] += 1
+        n = i//b
+        arr.append(n)
+
     
 sum = 0
 for num in rst:
