cf6766adcf65753c5e8b9552556b8ef0c0d88ff0
박성현 <101372694+KKDDDS@users.noreply.github.com>
2024-12-29 16:20:37 +0900

    Title: 알파벳 출력하기 2, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241229/알파벳 출력하기 2/print-alphabet-2.py
@@ -0,0 +1,11 @@
+N = int(input())
+cnt = 0
+for i in range(N):
+    for j in range(i):
+        print(" ",end=' ')
+    for k in range(N-i):
+        print(chr(65+cnt),end=' ')
+        cnt += 1
+        if cnt == 26:
+            cnt = 0
+    print()
