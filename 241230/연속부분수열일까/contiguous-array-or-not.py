2033d73253291f3eb582f8ed77256db5e082355c
박성현 <101372694+KKDDDS@users.noreply.github.com>
2024-12-30 21:16:11 +0900

    Title: 연속부분수열일까, Time: 40ms, Memory: 16MB, Status: Passed - CodeTree

--- 241230/연속부분수열일까/contiguous-array-or-not.py
+++ 241230/연속부분수열일까/contiguous-array-or-not.py
@@ -1,9 +1,10 @@
 n1, n2 = map(int, input().split())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
-n = 0
+
 ans = False
 for i in range(n1-n2+1):
+    n = 0
     while A[i] == B[n]:
         i += 1
         n += 1
