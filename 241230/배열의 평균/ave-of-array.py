29d0c4958f8ab7361a9d4c42dad033be6a9eff37
박성현 <101372694+KKDDDS@users.noreply.github.com>
2024-12-30 14:50:44 +0900

    Title: 배열의 평균, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 241230/배열의 평균/ave-of-array.py
+++ 241230/배열의 평균/ave-of-array.py
@@ -9,7 +9,7 @@ for i in range(2):
     sum = 0
     for j in range(4):
         sum += arr[i][j]
-    print(f"{sum//4:.1f}",end=' ')
+    print(f"{sum/4:.1f}",end=' ')
 
 print()
 
@@ -17,11 +17,11 @@ for k in range(4):
     sum = 0
     for l in range(2):
         sum += arr[l][k]
-    print(f"{sum//2:.1f}", end=' ')
+    print(f"{sum/2:.1f}", end=' ')
 
 print()
 sum = 0
 for x in range(2):    
     for y in range(4):
         sum += arr[x][y]
-print(f"{sum//8:.1f}")
+print(f"{sum/8:.1f}")
