e7c8f0554194e6a6623bb95e5752a62b3f71b561
박성현 <101372694+KKDDDS@users.noreply.github.com>
2024-12-30 21:11:54 +0900

    Title: 연속부분수열일까, Time: 76ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 241230/연속부분수열일까/contiguous-array-or-not.py
@@ -0,0 +1,17 @@
+n1, n2 = map(int, input().split())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+n = 0
+ans = False
+for i in range(n1-n2+1):
+    while A[i] == B[n]:
+        i += 1
+        n += 1
+        if A[i] == B[-1]:
+            ans = True
+            break
+
+if ans==True:
+    print("Yes")
+else:
+    print("No")
