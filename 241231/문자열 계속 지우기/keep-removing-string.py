4f9290840d6fedc5d4dec6446cc6b5b628429278
박성현 <101372694+KKDDDS@users.noreply.github.com>
2024-12-31 18:21:23 +0900

    Title: 문자열 계속 지우기, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 241231/문자열 계속 지우기/keep-removing-string.py
+++ 241231/문자열 계속 지우기/keep-removing-string.py
@@ -1,20 +1,16 @@
 a = list(input())
 b = list(input())
 
-while len(a)>0:
-    for i in range(len(a)-len(b)+1):
-        cnt = 0
-        for j in range(len(b)):
-            if a[i+j] == b[j]:
-                cnt += 1
-
-        if cnt == len(b):
-            for k in range(len(b)):
-                a.pop(i)
+while True:
+    found = False  
+    for i in range(len(a) - len(b) + 1): 
+        if a[i:i+len(b)] == b:
+            del a[i:i+len(b)]
+            found = True 
             break
-            
-    if cnt != len(b):
-        ans = ''.join(a)
-        print(ans)
+    if not found:
         break
+
+ans = ''.join(a)  # 리스트를 문자열로 변환
+print(ans)
                 
