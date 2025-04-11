e7a0ad6a641e6e48fe7676f8b8349e879c9af492
박성현 <101372694+KKDDDS@users.noreply.github.com>
2025-04-11 16:36:25 +0900

    Title: 배열 채우고 출력하기, Time: 5ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250411/배열 채우고 출력하기/filling-array-and-print.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    char A[10] = {0};
+    int n = 10;
+    for(int i=0; i<10; i++){
+        cin >> A[i];
+    }
+    for(int i=9; i>=0; i--){
+        cout << A[i] << "";
+    }
+    return 0;
+}
