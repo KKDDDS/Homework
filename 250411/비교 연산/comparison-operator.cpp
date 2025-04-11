329e7f25aa202e612c480d1d7ae4ce2d98cefbe9
박성현 <101372694+KKDDDS@users.noreply.github.com>
2025-04-11 12:45:32 +0900

    Title: 비교 연산, Time: 5ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250411/비교 연산/comparison-operator.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int A, B;
+    cin >> A, B;
+    if(A>=B){
+        cout << true;
+    }
+    else{
+        cout << false;
+    }
+    if(A>B){
+        cout << "\n";
+        cout << true;
+    }
+    else{
+        cout << "\n";
+        cout << false;
+    }
+    if(A<=B){
+        cout << "\n";
+        cout << true;
+    }
+    else{
+        cout << "\n";
+        cout << false;
+    }
+    if(A<B){
+        cout << "\n";
+        cout << true;
+    }
+    else{
+        cout << "\n";
+        cout << false;
+    }
+    if(A==B){
+        cout << "\n";
+        cout << true;
+    }
+    else{
+        cout << "\n";
+        cout << false;
+    }
+    if(A!=B){
+        cout << "\n";
+        cout << true;
+    }
+    else{
+        cout << "\n";
+        cout << false;
+    }
+    return 0;
+}
