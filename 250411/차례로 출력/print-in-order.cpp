5ff70d76d0cf5dd9987ed1093cf0679bbf41df5d
박성현 <101372694+KKDDDS@users.noreply.github.com>
2025-04-11 14:34:12 +0900

    Title: 차례로 출력, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250411/차례로 출력/print-in-order.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 5;
+    int b = 17;
+    
+    for(a; a<=b; a++){
+        cout << a << " ";
+    }
+    return 0;
+}
