f6607d85c79b361b2bb66d804a05ee914533fecd
박성현 <101372694+KKDDDS@users.noreply.github.com>
2025-04-11 14:24:56 +0900

    Title: 문자열 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250411/문자열 출력하기/print-string.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int i, N;
+    cin >> N;
+    for(i=0;i<N;i++){
+        cout << "LeebrosCode"<<"\n";
+    }
+    return 0;
+}
