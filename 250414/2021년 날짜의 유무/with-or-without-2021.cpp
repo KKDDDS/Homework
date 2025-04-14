091392dabccc343a5f4211fc5605e466389bbc11
박성현 <101372694+KKDDDS@users.noreply.github.com>
2025-04-14 12:25:19 +0900

    Title: 2021년 날짜의 유무, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250414/2021년 날짜의 유무/with-or-without-2021.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <array>
+
+using namespace std;
+
+int M, D;
+
+int main() {
+    cin >> M >> D;
+
+    // Please write your code here.
+    array<int, 7> arr = {1,3,5,7,8,10,12};
+    bool is_in = false;
+    for (int i = 0; i < arr.size(); i++) {
+        if (arr[i] == M) {
+            is_in = true;
+            break;
+        }
+    }
+    if(is_in){
+        if(D<=31){
+            cout << "Yes";
+            return 0;
+        }
+    }
+    else if(M==2){
+        if(D<=28){
+            cout << "Yes";
+            return 0;
+        }
+    }
+    else{
+        if(D<=30){
+            cout << "Yes";
+            return 0;
+        }
+    }
+    cout << "No";
+    return 0;
+}
